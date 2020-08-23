981e1a5ac093becf28900b10df3d95c950799506
claytonfaria <claytonfaria@gmail.com>
2020-08-23 14:38:55 +0800

    bootcamp CRUD ok

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
+/* eslint-disable space-before-function-paren */
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   });
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
